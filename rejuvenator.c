a3651864d9b2ee66f08a5c3a4ad302f3816fdd3b
herochen11 <hero211cs@gmail.com>
2022-06-05 16:39:44 +0800

    add  multiiple functions

--- rejuvenator.c
+++ rejuvenator.c
@@ -3,7 +3,9 @@
 #include <string.h>
 #include <assert.h>
 
-#define FBL_NOT_IN_LIST			(-2)		//* mean this block is not in the free block list
+#define CLEAN					(-1)
+#define INVALID					(-2)
+#define FBL_NOT_IN_LIST			(-3)		//* mean this block is not in the free block list
 #define MAXBLOCK 				128			// 2^6
 #define BYTE_PER_PAGE 			64
 #define PAGE_PER_BLOCK 			64
@@ -12,196 +14,311 @@
 #define MAX_ERASE_CNT			100
 typedef int	 flash_size_t;
 
-char* Memory;
+/*
+	flash memory
+*/ 
+flash_size_t InvalidPageCnt[MAXBLOCK];		//* the number of invalid pages in each block
+flash_size_t ValidPageCnt[MAXBLOCK];		//* the number of valid pages in each block
+flash_size_t CleanPageCnt[MAXBLOCK];		//* the number of clean pages in each block
+flash_size_t BlockEraseCnt[MAXBLOCK];		//* the number of earse count of each block
+// flash_size_t CurrentFreeBlockID;			//* Point to current free block ID. If the garbage collection is called, this value will always be -1.
+flash_size_t **FlashMemory;  //* -1: clean, -3: invalid, other : the LBA is stored in this page
+flash_size_t PageMappingTable[PAGE_PER_BLOCK*MAXBLOCK];	 //* LBA to PBA, assume 1-to-1 mapping, -1 means no mapping
+flash_size_t LowFreeBlockList[MAXBLOCK];		//* the free block list array
+flash_size_t HighFreeBlockList[MAXBLOCK];		//* the free block list array  	
+flash_size_t LowFreeBlockListHead;
+flash_size_t LowFreeBlockListTail;
+flash_size_t HighFreeBlockListHead;
+flash_size_t HighFreeBlockListTail;	
 
 
-
-flash_size_t InvalidPageCnt[MAXBLOCK] = {0};	//* store the number of invalid pages in each block
-flash_size_t ValidPageCnt[MAXBLOCK] = {0};		//* store the number of valid pages in each block
-flash_size_t FreeBlockList[MAXBLOCK] = {-2};	//* the free block list array
-flash_size_t CurrentFreeBlockID = -1;			//* Point to current free block ID. If the garbage collection is called, this value will always be -1.
-
-// LPN to PPN, assume 1-to-1 mapping, -1 means no mapping
-flash_size_t PageMappingTable[PAGE_PER_BLOCK*MAXBLOCK]	= {-1};	
-
-
-// rejuvenator block list
+/*
+	Rejuvenator block list
+*/ 
 flash_size_t Tau = 16;
+flash_size_t M;
 flash_size_t MinWear = 0;
-flash_size_t MaxWear = 16;
+flash_size_t MaxWear = 0;
 flash_size_t LowCleanBlockCnt;
 flash_size_t HighCleanBlockCnt;
 flash_size_t CleanBlockCnt;
-flash_size_t M;
-flash_size_t BeginOfBlockListOffset[MAX_ERASE_CNT] = {0};		// if BeginOfBlockListOffset[cnt] > EndOfBlockListOffset[cnt]
-flash_size_t EndOfBlockListOffset[MAX_ERASE_CNT] = {0};			// there is no block in cnt.    #cnt : the erase count
-flash_size_t OffsetInBlockList[MAXBLOCK] = {0};
-flash_size_t EraseCnt[MAXBLOCK] = {0};
-flash_size_t RejuBlockList[MAXBLOCK]= {-1}; 
-//flash_size_t RejuBlockList[MAX_ERASE_CNT][MAXBLOCK]= {-1}; //the index of RejuBlockList indeicate the erase count
+flash_size_t BeginOfBlockListOffset[MAX_ERASE_CNT+1] = {0};		// The begin offset of "erase count" in the RejuBlockList.
+flash_size_t EndOfBlockListOffset[MAX_ERASE_CNT+1] = {0};	    // The end offset of "erase count" in the RejuBlockList.
+flash_size_t OffsetInBlockList[MAXBLOCK] = {0};					//The offset of "BlockID" in the RejuBlockList. 
+flash_size_t RejuBlockList[MAXBLOCK];		
+flash_size_t HighActiveBlockPtr, LowActiveBlockPtr;
+flash_size_t HighActivePagePtr, LowActivePagePtr;
 
-flash_size_t HighActivePtr, LowActivePtr;
-
-// assume p1 and p2 are both byte addr
-void copyPage(flash_size_t p1, flash_size_t p2){
-	for(flash_size_t i=0; i<BYTE_PER_PAGE; ++i){
-		Memory[p2+i] = Memory[p1+i];
-	}
-}
-
+/*
+	Second Chance LRU
+*/
 int FTLIsHotPage(flash_size_t addr){
 	assert(addr > 0);
 	return 1;
 }
 
-int LBAtoPBA(){}
-
-int PBAtoLBA(){}
-
-int FTLIsValidPage(flash_size_t p){
-	return 1;
+// assume pba1 and pba2 are both byte addr. pba is physical block address.
+void copyPage(flash_size_t pba, flash_size_t dst_block,flash_size_t dst_page){
+	flash_size_t src_block = pba/PAGE_PER_BLOCK; 
+	flash_size_t src_page =  (pba % PAGE_PER_BLOCK)/BYTE_PER_PAGE;
+	FlashMemory[dst_block][dst_page] = FlashMemory[src_block][src_page];
 }
 
-// update HighActivePtr and LowActivePtr
-    void FTLUpdateActivePtr(){
-
+int LBAtoPBA(flash_size_t lba){
+	return PageMappingTable[lba];
 }
 
+int FTLIsValidPage(flash_size_t pba){
+	flash_size_t blockId = pba/PAGE_PER_BLOCK; 
+	flash_size_t offset =  (pba % PAGE_PER_BLOCK)/BYTE_PER_PAGE;
+	if(FlashMemory[blockId][offset] != INVALID && FlashMemory[blockId][offset] != CLEAN)
+		return 1;
+	else 
+		return 0;
+}
 
 // invalidate page p
 // its block = p/PAGE_PER_BLOCk/BYTE_PER_PAGE
 // update invalid page of block
-void FTLInvalidatePage(flash_size_t p){
-	
+void FTLInvalidatePage(flash_size_t pba){
+	flash_size_t block = pba/PAGE_PER_BLOCK; 
+	flash_size_t page =  (pba % PAGE_PER_BLOCK)/BYTE_PER_PAGE;
+	FlashMemory[block][page] = INVALID;	
 }
 
-void FTLValidatePage(flash_size_t p){
-
+void FTLValidatePage(flash_size_t block, flash_size_t page){
+	FlashMemory[block][page] = block;	
 }
 
-// update Page mapping table, if PMT entry (e, p1) => (e, p2)
-void FTLUpdatePageTable(flash_size_t p1, flash_size_t p2){
+void FTLUpdateActivePtr(){
+	// update LowActivePtr
+	LowActiveBlockPtr = LowFreeBlockList[LowFreeBlockListHead];
+	if( LowActivePagePtr >= PAGE_PER_BLOCK )
+	{
+		LowActivePagePtr = 0;
+		LowFreeBlockListHead++;
+		if(LowFreeBlockListHead < MAXBLOCK)
+		{
+			LowActiveBlockPtr = LowFreeBlockList[LowFreeBlockListHead];
+		}
+		else
+		{	
+			LowFreeBlockListHead = 0;
+			LowActiveBlockPtr = LowFreeBlockList[LowFreeBlockListHead];
+		}
 
+	}
+
+	// update HighActivePtr
+	if( MaxWear <= M )  //This condition means all blocks are in lower numbered list
+	{
+		HighActiveBlockPtr = LowActiveBlockPtr;
+		HighActivePagePtr = LowActivePagePtr;
+	}
+	else
+	{ 
+		HighActiveBlockPtr = HighFreeBlockList[HighFreeBlockListHead];
+		if( HighActivePagePtr >= PAGE_PER_BLOCK )
+		{
+			HighActivePagePtr = 0;
+			HighFreeBlockListHead++;
+			if(HighFreeBlockListHead < MAXBLOCK)
+			{
+				HighActiveBlockPtr = HighFreeBlockList[HighFreeBlockListHead];
+			}
+			else
+			{	
+				HighFreeBlockListHead = 0;
+				HighActiveBlockPtr = HighFreeBlockList[HighFreeBlockListHead];
+			}
+
+		}
+
+	}
 }
 
-int FTLIsValidPage(flash_size_t p){
-	return 1;
+// update Page mapping table, if PMT entry (lba, pba1) => (bla, pba2)
+void FTLUpdatePageTable(flash_size_t pba, flash_size_t new_block, flash_size_t new_page){
+	flash_size_t old_block = pba/PAGE_PER_BLOCK; 
+	flash_size_t old_page =  (pba % PAGE_PER_BLOCK)/BYTE_PER_PAGE;
+
+	PageMappingTable[FlashMemory[old_block][old_page]] = new_block * BLOCK_BYTE_SIZE + new_page * BYTE_PER_PAGE;
+	FlashMemory[new_block][new_page] =  FlashMemory[old_block][old_page];
 }
 
+void UpdateRejuParameter(){
+	//TODO
+	//update Tau
+	M = Tau/2;
+}
 
-void RejuInitBlockList(){
+void InitFlashMemory(){
+
+	FlashMemory = (flash_size_t**) malloc(sizeof(flash_size_t*)*MAXBLOCK);
+	for( int i=0; i < MAXBLOCK; i++)
+	{
+		FlashMemory[i] = (flash_size_t *)malloc(sizeof(flash_size_t)*PAGE_PER_BLOCK);
+		memset(FlashMemory[i],CLEAN,sizeof(flash_size_t)*PAGE_PER_BLOCK);	//* initialize the content of each page: means there is nothing in any page initially.
+	}
+
+	memset(PageMappingTable, -1, sizeof(PageMappingTable));
+	memset(ValidPageCnt, 0, sizeof(ValidPageCnt));
+	memset(InvalidPageCnt, 0, sizeof(InvalidPageCnt));
+	memset(BlockEraseCnt, 0, sizeof(BlockEraseCnt));
+
+	for(flash_size_t i=0; i<MAXBLOCK; i++){
+		CleanPageCnt[i] = PAGE_PER_BLOCK;
+		LowFreeBlockList[i] = i;
+		HighFreeBlockList[i] = FBL_NOT_IN_LIST;
+	}
+	LowFreeBlockListHead = 0;
+	LowFreeBlockListTail = MAXBLOCK-1;
+	HighFreeBlockListHead = -1;
+	HighFreeBlockListTail = -1;
+	//CurrentFreeBlockID = 0;
+}
+
+void InitRejuBlockList(){
 	// put all blocks in block list with erase_cnt = 0
 	for(flash_size_t i=0; i<MAXBLOCK; ++i){
         RejuBlockList[i] = i;
-		//RejuBlockList[0][i] = i;
+		OffsetInBlockList[i] = i; 
 	}
- 
+	
+	// if BeginOfBlockListOffset[cnt] > EndOfBlockListOffset[cnt]
+	// there is no block in cnt.    #cnt : the erase count
+	BeginOfBlockListOffset[0] = 0;
 	EndOfBlockListOffset[0] = MAXBLOCK-1;
-	for(flash_size_t i=1; i<MAX_ERASE_CNT; ++i){
+	for(flash_size_t i=1; i<MAX_ERASE_CNT; ++i){ 
 		BeginOfBlockListOffset[i] = MAXBLOCK;
 		EndOfBlockListOffset[i] = MAXBLOCK-1;
 	}
 	LowCleanBlockCnt = MAXBLOCK;
 	HighCleanBlockCnt = 0;
 	CleanBlockCnt = MAXBLOCK;
-	M = (MaxWear+MinWear)/2;
+	MinWear = MaxWear = 0;
+	M = Tau/2;
 }
 
-//* ****************************************************************
-//* Erase one block
-//* ****************************************************************
+
+void PutFreeBlock(flash_size_t BlockID)
+{
+
+	if(	BlockEraseCnt[BlockID] > M )
+	{	
+		if(HighCleanBlockCnt == 0)
+		{
+			HighFreeBlockListHead = 0;
+			HighFreeBlockList[HighFreeBlockListHead] = BlockID;
+		}
+		
+		if( (HighFreeBlockListTail + 1) == MAXBLOCK)
+			HighFreeBlockListTail = 0;
+		else 
+			HighFreeBlockListTail++;
+		HighFreeBlockList[HighFreeBlockListTail] = BlockID;
+	}
+	else
+	{	
+		if( (LowFreeBlockListTail + 1) == MAXBLOCK)
+			LowFreeBlockListTail = 0;
+		else 
+			LowFreeBlockListTail++;
+		LowFreeBlockList[LowFreeBlockListTail] = BlockID;
+	} 
+
+}
+
 void FTLEraseOneBlock(flash_size_t BlockID){
 	assert(BlockID < MAXBLOCK);
 	// physically erase this Block
-	flash_size_t bOffset = BlockID*BLOCK_BYTE_SIZE;
-	for(flash_size_t i=bOffset; i<BLOCK_BYTE_SIZE; ++i)
-		Memory[i] = 0;
-
-	// then ...
-
+	for(flash_size_t i=0; i<PAGE_PER_BLOCK; ++i)
+		FlashMemory[BlockID][i] = CLEAN;
+	
 	InvalidPageCnt[BlockID] = 0;
 	ValidPageCnt[BlockID] = 0;
-	flash_size_t eraseCnt = EraseCnt[BlockID];
+	CleanPageCnt[BlockID] = PAGE_PER_BLOCK;
+	flash_size_t eraseCnt = BlockEraseCnt[BlockID];
 	if(eraseCnt == MAX_ERASE_CNT) return;
 
-	flash_size_t pos = OffsetInBlockList[BlockID];
-	flash_size_t end = EndOfBlockListOffset[eraseCnt]; // end offset
+	//move the erase block to head of next erase count list
+	flash_size_t pos = OffsetInBlockList[BlockID]; 		//the offset of the block in the RejuBlockList
+	flash_size_t end = EndOfBlockListOffset[eraseCnt];  // the end offset of the "erasecnt" in the RejuBlockList
 	assert(pos <= end);
 	flash_size_t swappedBlock = RejuBlockList[end];
 	RejuBlockList[end] = BlockID;
 	RejuBlockList[pos] = swappedBlock;
-	++EraseCnt[BlockID];
-	assert(EndOfBlockListOffset[eraseCnt] > 0);
-	--EndOfBlockListOffset[eraseCnt];
+
+	//update erase count and BlockListOffset
+	++BlockEraseCnt[BlockID];			
+	--EndOfBlockListOffset[eraseCnt];		
 	--BeginOfBlockListOffset[eraseCnt+1];
 
-	if(eraseCnt+1 >= M)
+	//put the block into free block list..
+	PutFreeBlock(BlockID);
+	
+	//update minwear and maxwear
+	if(BlockEraseCnt[BlockID]  > MaxWear) MaxWear = eraseCnt;
+	if(EndOfBlockListOffset[MinWear] == -1) MinWear++;
+	assert((MaxWear - MinWear) <= Tau );
+
+	if(eraseCnt+1 > M)
 		++HighCleanBlockCnt;
 	else	
-		++LowCleanBlockCnt;
+		++LowCleanBlockCnt;	
+	
 	++ CleanBlockCnt;
-	//updateRejuParameter
+	
 }
 
-//# ****************************************************************
-//# Do garbage collection to reclaim one more free block if there is no free block in the free block list
-//# dynamic wear leveling: using cost-benefit strategy for its GC->
-//# (free page=0, live page=-1, dead page = 1) while the sum over every page is larger than 0, erase the block.
-//# Always reclaim the first block that has the maximal number of invalid pages
-//# ****************************************************************
-void FTLGarbageCollection()
-{
-	// find victim block with maximum cleaning efficiency in the lower numbered list 
-	// that is, the block with minimum valid page
+
+void FTLGarbageCollection(){
+	// find victim block with maximum cleaning efficiency in the lower numbered list
 	flash_size_t minValidPageCnt = PAGE_PER_BLOCK+1;
 	flash_size_t victimBlock = -1;
-	for(flash_size_t i=0; i<M; ++i){  
-		flash_size_t j = BeginOfBlockListOffset[i];
+	for(flash_size_t i=MinWear; i<M; ++i){  //min_wear to min_wear + M  
+		flash_size_t begin = BeginOfBlockListOffset[i];
 		flash_size_t end = EndOfBlockListOffset[i];
 		if(end==MAXBLOCK) continue; // empty list in RejBlockList
-		for(; j <= end; ++j){
-			flash_size_t b = RejuBlockList[j];
-			if( ValidPageCnt[b] < minValidPageCnt ){
+		for(; begin <= end; ++begin){
+			flash_size_t b = RejuBlockList[begin];
+			if( ValidPageCnt[b] < minValidPageCnt && LowFreeBlockList[b] == FBL_NOT_IN_LIST){
 				minValidPageCnt = ValidPageCnt[b];
 				victimBlock = b;
 			}
 		}
-	}
 
-	//NOTE  clean block can be victim block ?
-	if(victimBlock==-1) return;
+	assert(victimBlock != -1);
 
-	flash_size_t bOffset = victimBlock*BLOCK_BYTE_SIZE;
+	//live page copy
+	flash_size_t physicalOffset = victimBlock*BLOCK_BYTE_SIZE; //physical offset in the memory 
+	flash_size_t bOffset = 0; //the offest in the victim block (unit is page)
 	for(flash_size_t p=bOffset; p+=BYTE_PER_PAGE; p<bOffset + BLOCK_BYTE_SIZE){
-		if( ! FTLIsValidPage(p) ) continue;
+		if( !FTLIsValidPage(p) ) continue;
 		FTLUpdateActivePtr();
 		if( FTLIsHotPage(p) ){
-			copyPage(p, LowActivePtr);
-			FTLValidatePage(LowActivePtr);
-			//update 
-			// NOTE How to check which LPN map to p
+			copyPage(p, LowActiveBlockPtr, LowActivePagePtr);
+			FTLValidatePage(LowActiveBlockPtr, LowActivePagePtr);
+			FTLUpdatePageTable(p, LowActiveBlockPtr, LowActivePagePtr);
+			LowActivePagePtr++;
 		}
 		else{
-			copyPage(p, HighActivePtr);
-			FTLValidatePage(HighActivePtr);
-			//update 
-		}
-		
-		FTLInvalidatePage(p);
-		
+			copyPage(p, HighActiveBlockPtr, HighActivePagePtr);
+			FTLValidatePage(HighActiveBlockPtr, HighActivePagePtr);
+			FTLUpdatePageTable(p, HighActiveBlockPtr, HighActivePagePtr);
+			HighActivePagePtr++;
+		}	
+		FTLInvalidatePage(p);	
 	}
 
 	//erase block
-	++LowCleanBlockCnt;
-	++CleanBlockCnt;
+	FTLEraseOneBlock(victimBlock);
+	UpdateRejuParameter();
 
 }
 
-
-
 int main()
 {
-	Memory = (char*) malloc( DISK_BYTE_SIZE * sizeof(char) );
-	memset(Memory, 0, DISK_BYTE_SIZE);
-	return 0;
+	InitFlashMemory();
+	InitRejuBlockList();
 }
